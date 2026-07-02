fa2e6a677e6ac26455da9694eebc0ab48c8f0311
Tarun Sharma <tarundadhich05@gmail.com>
2026-07-02 23:16:18 +0530

    - feat(patterns): added invertedRightAngleTriangle solution, rightAngleTriangle solution

--- src/02_patterns/Day0.java
+++ src/02_patterns/Day0.java
@@ -1,13 +1,19 @@
-public class Day0{
-    public static void main(String[] args){
+public class Day0 {
+    public static void main(String[] args) {
+        System.out.println("Mirrored Right Angle Triangle");
+        mirroredRightAngleTriangle(4);
 
-         pattern1(  4);
+        System.out.println("\nRight Angle Triangle");
+        rightAngleTriangle(5);
+
+        System.out.println("\nInverted Right Angle Triangle");
+        invertedRightAngleTriangle(5);
     }
 
 
-    static void pattern1(int n){
-        for (int row = 1; row <= n; row++){
-            for (int col = 1; col <= n; col++){
+    static void pattern1(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -15,16 +21,38 @@ public class Day0{
     }
 
     //self
-    static void pattern6(int n){
-        for(int row = 1; row <=n; row++){
-            for (int col = 1; col <= n-row; col++) {
+    static void mirroredRightAngleTriangle(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n - row; col++) {
                 System.out.print(" ");
             }
-            for (int str = 1; str <= row; str++){
+            for (int str = 1; str <= row; str++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
 
+
+    static void rightAngleTriangle(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+
+    static void invertedRightAngleTriangle(int n){
+        for(int row = 1; row <= n; row++){
+            for(int col = 1; col <= n-row+1; col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
+
+
+
