eb2f2adc8c778c56e4e633f78f3365eacdafb716
Tarun Sharma <tarundadhich05@gmail.com>
2026-07-02 22:36:14 +0530

    - feat(patterns): added box star pattern solution

--- src/02_patterns/Day0.java
+++ src/02_patterns/Day0.java
@@ -1,7 +1,17 @@
 public class Day0{
     public static void main(String[] args){
 
-         pattern6(  4);
+         pattern1(  4);
+    }
+
+
+    static void pattern1(int n){
+        for (int row = 1; row <= n; row++){
+            for (int col = 1; col <= n; col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     //self
